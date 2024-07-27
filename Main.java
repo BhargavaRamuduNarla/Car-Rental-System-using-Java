4e489be760c6443c6bbdd842f92fe8446875a71b
BhargavaRamuduNarla <bhargavnarla770@gmail.com>
2024-07-27 11:18:34 +0530

    Updated price and list of cars Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-//
 
 class Car {
     private String carId;
@@ -245,9 +244,10 @@ public class Main{
     public static void main(String[] args) {
         CarRentalSystem rentalSystem = new CarRentalSystem();
 
-        Car car1 = new Car("C001", "Toyota", "Camry", 60.0); // Different base price per day for each car
-        Car car2 = new Car("C002", "Honda", "Accord", 70.0);
-        Car car3 = new Car("C003", "Mahindra", "Thar", 150.0);
+        // List of different base price per day for each car
+        Car car1 = new Car("C001", "Toyota", "Fortuner", 10000.0); 
+        Car car2 = new Car("C002", "Swift", "Dzire", 2000.0);
+        Car car3 = new Car("C003", "Mahindra", "Thar", 6000.0);
         rentalSystem.addCar(car1);
         rentalSystem.addCar(car2);
         rentalSystem.addCar(car3);
